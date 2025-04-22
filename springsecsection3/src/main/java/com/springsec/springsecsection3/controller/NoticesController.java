f780038971ef05f6f05e29218e6fb78bd55e2711
Prateek Gupta <prateekgupta2002@gmail.com>
2025-04-22 11:46:14 +0530

    Configured users using InmemoryUserDetailsManager, configured Password Encoder using PasEncFactories, CompromisedPasswordChecker

---
+++ springsecsection3/src/main/java/com/springsec/springsecsection3/controller/NoticesController.java
@@ -0,0 +1,12 @@
+package com.springsec.springsecsection3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+    @GetMapping("/notices")
+    public String getNotices(){
+        return "notices details from db";
+    }
+}

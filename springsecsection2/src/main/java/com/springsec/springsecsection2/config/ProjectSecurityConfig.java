f32b32d4636e76cd3095c8471af75281e40cbb5e
Prateek Gupta <prateekgupta2002@gmail.com>
2025-04-22 11:44:32 +0530

    .

--- springsecsection2/src/main/java/com/springsec/springsecsection2/config/ProjectSecurityConfig.java
+++ springsecsection2/src/main/java/com/springsec/springsecsection2/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
